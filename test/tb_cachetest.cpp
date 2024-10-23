aa4e665b7108bc04d666adeffa6ae319877eb06f
ben <greatmaharg@gmail.com>
2024-10-22 21:55:11 -0400

    Dubious changes to cachetest tb

--- test/tb_cachetest.cpp
+++ test/tb_cachetest.cpp
@@ -44,24 +44,22 @@ int main(int argc, char** argv) {
             dut.addr_b = 0x0004;
             dut.datain_b = 0x1234;
             stepclk(&trace);
-            dut.valid_b = 0;
-            dut.wr_b = 0;
-            dut.addr_b = 0xEEEE;
-            dut.datain_b = 0xEEEE;
-            dut.eval();
             // Wait for memory to report ready
             for(size_t count = 0; !dut.ready_b; count++) {
                 stepclk(&trace);
                 tc.assertLess(count, 10, "Took too long to write");
             }
+            dut.valid_b = 0;
+            dut.wr_b = 0;
+            dut.addr_b = 0xEEEE;
+            dut.datain_b = 0xEEEE;
+            dut.eval();
+            
 
             // Read data back from memory
             dut.valid_b = 1;
             dut.addr_b = 0x0004;
             stepclk(&trace);
-            dut.valid_b = 0;
-            dut.addr_b = 0xEEEE;
-            dut.eval();
             // Wait for memory to report ready
             for(size_t count = 0; !dut.ready_b; count++) {
                 stepclk(&trace);
@@ -69,6 +67,9 @@ int main(int argc, char** argv) {
             }
 
             tc.assertEqual(dut.dataout_b, 0x1234, "Incorrect data returned");
+            dut.valid_b = 0;
+            dut.addr_b = 0xEEEE;
+            dut.eval();
         }
 
         {
@@ -81,29 +82,29 @@ int main(int argc, char** argv) {
             dut.addr_b = 0x0014;
             dut.datain_b = 0x2345;
             stepclk(&trace);
-            dut.valid_b = 0;
-            dut.wr_b = 0;
-            dut.addr_b = 0xEEEE;
-            dut.datain_b = 0xEEEE;
-            dut.eval();
             // Wait for memory to report ready
             for(size_t count = 0; !dut.ready_b; count++) {
                 stepclk(&trace);
                 tc.assertLess(count, 10, "Took too long to write");
             }
+            dut.valid_b = 0;
+            dut.wr_b = 0;
+            dut.addr_b = 0xEEEE;
+            dut.datain_b = 0xEEEE;
+            dut.eval();
 
             // Read data back from memory
             dut.valid_b = 1;
             dut.addr_b = 0x0014;
             stepclk(&trace);
-            dut.valid_b = 0;
-            dut.addr_b = 0xEEEE;
-            dut.eval();
             // Wait for memory to report ready
             for(size_t count = 0; !dut.ready_b; count++) {
                 stepclk(&trace);
                 tc.assertLess(count, 10, "Took too long to read");
             }
+            dut.valid_b = 0;
+            dut.addr_b = 0xEEEE;
+            dut.eval();
 
             tc.assertEqual(dut.dataout_b, 0x2345, "Incorrect data returned");
             dut.valid_b = 1;
@@ -123,45 +124,45 @@ int main(int argc, char** argv) {
             dut.addr_b = 0x0024;
             dut.datain_b = 0x3456;
             stepclk(&trace);
-            dut.valid_b = 0;
-            dut.wr_b = 0;
-            dut.addr_b = 0xEEEE;
-            dut.datain_b = 0xEEEE;
-            dut.eval();
             // Wait for memory to report ready
             for(size_t count = 0; !dut.ready_b; count++) {
                 stepclk(&trace);
                 tc.assertLess(count, 10, "Took too long to write");
             }
+            dut.valid_b = 0;
+            dut.wr_b = 0;
+            dut.addr_b = 0xEEEE;
+            dut.datain_b = 0xEEEE;
+            dut.eval();
             // Write more data to memory, adjacent to existing item
             dut.valid_b = 1;
             dut.wr_b = 1;
             dut.addr_b = 0x0028;
             dut.datain_b = 0x4567;
             stepclk(&trace);
-            dut.valid_b = 0;
-            dut.wr_b = 0;
-            dut.addr_b = 0xEEEE;
-            dut.datain_b = 0xEEEE;
-            dut.eval();
             // Wait for memory to report ready
             for(size_t count = 0; !dut.ready_b; count++) {
                 stepclk(&trace);
                 tc.assertLess(count, 10, "Took too long to write");
             }
+            dut.valid_b = 0;
+            dut.wr_b = 0;
+            dut.addr_b = 0xEEEE;
+            dut.datain_b = 0xEEEE;
+            dut.eval();
 
             // Read data back from memory
             dut.valid_b = 1;
             dut.addr_b = 0x0024;
             stepclk(&trace);
-            dut.valid_b = 0;
-            dut.addr_b = 0xEEEE;
-            dut.eval();
             // Wait for memory to report ready
             for(size_t count = 0; !dut.ready_b; count++) {
                 stepclk(&trace);
                 tc.assertLess(count, 10, "Took too long to read");
             }
+            dut.valid_b = 0;
+            dut.addr_b = 0xEEEE;
+            dut.eval();
 
             tc.assertEqual(dut.dataout_b, 0x3456, "Incorrect data returned");
             dut.valid_b = 1;
@@ -180,12 +181,13 @@ int main(int argc, char** argv) {
             dut.wr_b = 1;
             dut.datain_b = 0x5678;
             stepclk(&trace);
-            dut.valid_b = 0;
             // Wait for memory to report ready
             for(size_t count = 0; !dut.ready_b; count++) {
                 stepclk(&trace);
                 tc.assertLess(count, 10, "Took too long to write");
             }
+            dut.valid_b = 0;
+            stepclk(&trace);
 
             dut.valid_b = 1;
             dut.addr_b = 0x0024;
@@ -205,12 +207,12 @@ int main(int argc, char** argv) {
             dut.valid_b = 1;
             dut.addr_b = 0x0014;
             stepclk(&trace);
-            dut.valid_a = 0;
-            dut.valid_b = 0;
             for(size_t count = 0; !dut.ready_b; count++) {
                 stepclk(&trace);
                 tc.assertLess(count, 10, "Took too long to read");
             }
+            dut.valid_a = 0;
+            dut.valid_b = 0;
             tc.assertEqual(dut.dataout_b, 0x5678, "Incorrect data on B");
             tc.assertEqual(dut.ready_a, 0, "B port should be fetched first");
 
@@ -239,45 +241,46 @@ int main(int argc, char** argv) {
             dut.wr_b = 1;
             dut.datain_b = 0x5678;
             stepclk(&trace);
-            dut.valid_b = 0;
             // Wait for memory to report ready
             for(size_t count = 0; !dut.ready_b; count++) {
                 stepclk(&trace);
                 tc.assertLess(count, 10, "Took too long to write");
             }
+            dut.valid_b = 0;
+            stepclk(&trace);
 
             dut.valid_b = 1;
             dut.addr_b = 0x0038;
             dut.wr_b = 1;
             dut.datain_b = 0x6789;
             stepclk(&trace);
-            dut.valid_b = 0;
-            dut.wr_b = 0;
             // Wait for memory to report ready
             for(size_t count = 0; !dut.ready_b; count++) {
                 stepclk(&trace);
                 tc.assertLess(count, 10, "Took too long to write");
             }
+            dut.valid_b = 0;
+            dut.wr_b = 0;
 
             dut.valid_a = 1;
             dut.addr_a = 0x0038;
             dut.valid_b = 1;
             dut.addr_b = 0x0034;
             stepclk(&trace);
-            dut.valid_a = 0;
-            dut.valid_b = 0;
             for(size_t count = 0; !dut.ready_b; count++) {
                 stepclk(&trace);
                 tc.assertLess(count, 10, "Took too long to read");
             }
             tc.assertEqual(dut.dataout_b, 0x5678, "Incorrect data on B");
             tc.assertEqual(dut.ready_a, 0, "B port should be fetched first");
-
             for(size_t count = 0; !dut.ready_a; count++) {
                 stepclk(&trace);
                 tc.assertLess(count, 10, "Took too long to read");
             }
             tc.assertEqual(dut.dataout_a, 0x6789, "Incorrect data on A");
+            dut.valid_a = 0;
+            dut.valid_b = 0;
+            stepclk(&trace);
         }
 
         {
@@ -289,36 +292,39 @@ int main(int argc, char** argv) {
             dut.wr_b = 1;
             dut.datain_b = 0x5678;
             stepclk(&trace);
-            dut.valid_b = 0;
             // Wait for memory to report ready
             for(size_t count = 0; !dut.ready_b; count++) {
                 stepclk(&trace);
                 tc.assertLess(count, 10, "Took too long to write");
             }
+            dut.valid_b = 0;
+            stepclk(&trace);
 
             dut.valid_b = 1;
             dut.addr_b = 0x0048;
             dut.wr_b = 1;
             dut.datain_b = 0x6789;
             stepclk(&trace);
-            dut.valid_b = 0;
-            dut.wr_b = 0;
             // Wait for memory to report ready
             for(size_t count = 0; !dut.ready_b; count++) {
                 stepclk(&trace);
                 tc.assertLess(count, 10, "Took too long to write");
             }
+            dut.valid_b = 0;
+            dut.wr_b = 0;
+            stepclk(&trace);
 
             // Prefetch
             dut.valid_b = 1;
             dut.addr_b = 0x0048;
             stepclk(&trace);
-            dut.valid_b = 0;
             // Wait for memory to report ready
             for(size_t count = 0; !dut.ready_b; count++) {
                 stepclk(&trace);
                 tc.assertLess(count, 10, "Took too long to prefetch");
             }
+            dut.valid_b = 0;
+            stepclk(&trace);
 
             dut.valid_a = 1;
             dut.addr_a = 0x0048;
